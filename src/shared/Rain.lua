a2f897a9e4f1689346162bcd7d99f543d50d598c
sergei <adrian.illumina@gmail.com>
2023-10-05 20:02:22 +0700

    changed rain module yo

--- src/shared/Rain.lua
+++ src/shared/Rain.lua
@@ -2,9 +2,35 @@ local RainModule = {}
 
 function RainModule.NewEmitterProperties()
     local EmitterProperties = {
+        PartProperties = {
+            Size = Vector3.new(10,1,10),
+            Transparency = 1
+        },
         Texture = "http://www.roblox.com/asset/?id=3806148993",
-        Orientation = nil
+        Orientation = Enum.ParticleOrientation.FacingCameraWorldUp,
+        EmissionDirection = Enum.NormalId.Bottom,
+        Speed = 50,
+        Rate = 50,
+        Lifetime = 1,
     }
+
+    return EmitterProperties
 end
+
+function RainModule.CreateEmitter(EmitterProperties)
+    local NewEmitterPart = Instance.new("Part")
+    NewEmitterPart.Size = EmitterProperties.PartProperties.Size
+    NewEmitterPart.Transparency = EmitterProperties.PartProperties.Transparency
+
+    local NewParticleEmitter = Instance.new("ParticleEmitter")
+    for Index, Value in pairs(EmitterProperties) do
+        NewParticleEmitter[Index] = Value
+    end
+
+    NewParticleEmitter.Parent = NewEmitterPart
+    return NewEmitterPart
+end
+
+
 
 return RainModule
