6f2ebfadaf2eaf625844dfe7b1d8825546c85d70
sergei <adrian.illumina@gmail.com>
2023-10-05 19:37:46 +0700

    umm yeah

---
+++ src/shared/Rain.lua
@@ -0,0 +1,10 @@
+local RainModule = {}
+
+function RainModule.NewEmitterProperties()
+    local EmitterProperties = {
+        Texture = "http://www.roblox.com/asset/?id=3806148993",
+        Orientation = nil
+    }
+end
+
+return RainModule
