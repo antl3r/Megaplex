653de3796c150f9f0c3d149e11fbd7a7565fb00a
sergei <adrian.illumina@gmail.com>
2023-10-05 20:30:40 +0700

    modified:   src/client/init.client.lua 	modified:   src/shared/Rain.lua

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,9 @@
-print("Hello world, from client!")
+local PlayerService = game:GetService("Players")
+
+local Shared = game:GetService("ReplicatedStorage"):WaitForChild("Shared")
+local RainModule = require(Shared:WaitForChild("Rain"))
+
+PlayerService.LocalPlayer.CharacterAdded:Connect(function(Character)
+    local Emitter = RainModule.CreateEmitter(RainModule.NewEmitterProperties())
+    RainModule.AttachEmitterPart(Emitter, Character)
+end)
--- src/shared/Rain.lua
+++ src/shared/Rain.lua
@@ -1,17 +1,20 @@
 local RainModule = {}
+local RunService = game:GetService("RunService")
+
+local Player = game:GetService("Players").LocalPlayer
 
 function RainModule.NewEmitterProperties()
     local EmitterProperties = {
         PartProperties = {
-            Size = Vector3.new(10,1,10),
+            Size = Vector3.new(20,1,20),
             Transparency = 1
         },
         Texture = "http://www.roblox.com/asset/?id=3806148993",
         Orientation = Enum.ParticleOrientation.FacingCameraWorldUp,
         EmissionDirection = Enum.NormalId.Bottom,
-        Speed = 50,
-        Rate = 50,
-        Lifetime = 1,
+        Speed = NumberRange.new(50,50),
+        Rate = 100,
+        Lifetime = NumberRange.new(1,1),
     }
 
     return EmitterProperties
@@ -21,16 +24,33 @@ function RainModule.CreateEmitter(EmitterProperties)
     local NewEmitterPart = Instance.new("Part")
     NewEmitterPart.Size = EmitterProperties.PartProperties.Size
     NewEmitterPart.Transparency = EmitterProperties.PartProperties.Transparency
+    NewEmitterPart.Anchored = true
 
     local NewParticleEmitter = Instance.new("ParticleEmitter")
-    for Index, Value in pairs(EmitterProperties) do
-        NewParticleEmitter[Index] = Value
+    for Index, Value in ipairs({
+        "Texture",
+        "Orientation",
+        "EmissionDirection",
+        "Speed",
+        "Rate",
+        "Lifetime"
+    })
+    do
+        NewParticleEmitter[Value] = EmitterProperties[Value]
     end
 
     NewParticleEmitter.Parent = NewEmitterPart
+    NewEmitterPart.Parent = game.Workspace
+    NewEmitterPart.Name = "doggydog"
+    print(NewEmitterPart)
     return NewEmitterPart
 end
 
-
+function RainModule.AttachEmitterPart(EmitterPart, Character)
+    RunService:BindToRenderStep("UpdateRainPosition", Enum.RenderPriority.Character.Value + 1,
+    function(Delta)
+        EmitterPart.CFrame = Character.PrimaryPart.CFrame * CFrame.new(0, 10, 0)
+    end)
+end
 
 return RainModule
