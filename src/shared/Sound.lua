3227305bccda202a35f2560f8a21b07fea22e02d
sergei <adrian.illumina@gmail.com>
2023-10-07 02:06:11 +0700

    modified:   src/client/init.client.lua 	deleted:    src/shared/Hello.lua 	modified:   src/shared/Rain.lua 	new file:   src/shared/Sound.lua 	modified:   src/shared/Util.lua

---
+++ src/shared/Sound.lua
@@ -0,0 +1,6 @@
+local SoundModule = {}
+--It's like sound service but not.
+
+
+
+return SoundModule
--- src/shared/Util.lua
+++ src/shared/Util.lua
@@ -4,6 +4,7 @@ function UtilModule:SetInstanceProperties(Instance, PropertyTable)
 	for Property, Value in pairs(PropertyTable) do
 		Instance[Property] = Value
 	end
+	return Instance
 end
 
 return UtilModule
